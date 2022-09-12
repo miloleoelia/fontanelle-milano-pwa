2ad2b56b300fc110af002cb32d54e38fcd0eaca7
Milo Maghini <milo.maghini@protonmail.com>
2022-09-12 14:40:34 +0200

    Refactoring

--- www/js/app.js
+++ www/js/app.js
@@ -1,33 +1,41 @@
-var map = L.map('map', {
-    attributionControl: false
-}).setView([45.4642, 9.1900], 13);
+let map = createMap();
+populateMapWithFountains(map);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    preferCanvas: true
-}).addTo(map);
 
-L.control.attribution({ position: 'topright' })
-    .addAttribution('© OpenStreetMap')
-    .addTo(map);
-
-fetch('./data/fountains-locations.json', {
-    method: 'get'
-})
-    .then(response => response.json())
-    .then(fountainsLocations => {
-        var markers = L.markerClusterGroup();
-        fountainsLocations.forEach(fountainLocation => {
-            markers.addLayer(
-                L.marker([
-                    fountainLocation.lat,
-                    fountainLocation.long
-                ])
-            );
+function populateMapWithFountains(map) {
+    fetch('./data/fountains-locations.json', { method: 'get' })
+        .then(response => response.json())
+        .then(fountainsLocations => {
+            var markers = L.markerClusterGroup();
+            fountainsLocations.forEach(fountainLocation => {
+                markers.addLayer(
+                    L.marker([
+                        fountainLocation.lat,
+                        fountainLocation.long
+                    ])
+                );
+            });
+            map.addLayer(markers);
+        })
+        .catch(err => {
+            window.alert('Errore nello scaricare i file necessari al funzionamento dell\'applicazione, riprovare piu tardi');
+            console.log(err);
         });
-        map.addLayer(markers);
-    })
-    .catch(err => {
-        window.alert('Errore nello scaricare i file necessari al funzionamento dell\'applicazione, riprovare piu tardi');
-        console.log(err);
-    });
+}
+
+function createMap() {
+    let map = L.map('map', {
+        attributionControl: false
+    }).setView([45.4642, 9.1900], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        preferCanvas: true
+    }).addTo(map);
+
+    L.control.attribution({ position: 'topright' })
+        .addAttribution('© OpenStreetMap')
+        .addTo(map);
+
+    return map;
+}
